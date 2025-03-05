cca403124eb854bdc0ee8718bafa2fd300b55d13
Sachin Sivakumar <ssk4988@gmail.com>
2025-03-05 17:39:16 -0500

    hello world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use leptos::prelude::*;
+
+fn main() {
+    console_error_panic_hook::set_once();
+    leptos::mount::mount_to_body(|| view! {
+        <p>
+            "Hello, World! 2"
+        </p>
+    })
+}
