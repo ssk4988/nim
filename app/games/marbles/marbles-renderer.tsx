5fcdf9cb1915bc2127908a96e54c14982fbf48d4
Sachin Sivakumar <ssk4988@gmail.com>
2025-04-28 14:38:45 -0400

    add multiplayer marbles

---
+++ app/games/marbles/marbles-renderer.tsx
@@ -0,0 +1,27 @@
+import { MarblesState } from "@/games/marbles";
+import { MarblesMove } from "@/types/marbles";
+
+export default function MarblesRenderer({ gameState, submitter }: { gameState: MarblesState, submitter: (move: MarblesMove) => void }) {
+    let stones = [];
+    let disabled = !gameState.turn;
+    for (let i = 1; i <= gameState.marbles; i++) {
+        stones.push(
+            <button
+                key={i}
+                className={`stone-button ${disabled || i > gameState.maxMarblesPerTurn ? "no-hover" : ""}`}
+                onClick={() => {
+                    console.log("Clicked stone:", i);
+                    if (disabled) return;
+                    let functionalAmount = Math.min(gameState.maxMarblesPerTurn, i);
+                    let move: MarblesMove = { amount: functionalAmount };
+                    submitter(move);
+                }}
+                disabled={disabled}
+            ></button>
+        );
+    }
+    stones.reverse();
+    return <div className="flex flex-row items-end justify-center gap-4 mt-8 min-h-[200px]">
+        {stones}
+    </div>
+}
