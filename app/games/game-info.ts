42e7057309c317d757f727c7cfe264f56c6ff23b
Sachin Sivakumar <ssk4988@gmail.com>
2025-05-07 21:18:20 -0400

    attempt to fix websocket server in prod

--- app/games/game-info.ts
+++ app/games/game-info.ts
@@ -3,6 +3,6 @@ import { displayGameType, GameTypeEnum } from "@/types/games";
 export const gameInfo = [
     { game: displayGameType(GameTypeEnum.MARBLES), path: '/games/marbles', description: 'Remove 1-3 marbles at a time', img: 'marbles', gameType: GameTypeEnum.MARBLES },
     { game: displayGameType(GameTypeEnum.NIM), path: '/games/nim', description: 'Remove stones from any pile', img: 'nim', gameType: GameTypeEnum.NIM },
-    { game: displayGameType(GameTypeEnum.LONE_KNIGHT), path: '/games/loneknight', description: 'Move one knight', img: 'loneknight', gameType: GameTypeEnum.LONE_KNIGHT },
-    { game: displayGameType(GameTypeEnum.MULTI_KNIGHT), path: '/games/multiknight', description: 'Move multiple knights', img: 'multiknight', gameType: GameTypeEnum.MULTI_KNIGHT },
+    { game: displayGameType(GameTypeEnum.LONE_KNIGHT), path: '/games/loneknight', description: 'Knight game with 1 knight', img: 'loneknight', gameType: GameTypeEnum.LONE_KNIGHT },
+    { game: displayGameType(GameTypeEnum.MULTI_KNIGHT), path: '/games/multiknight', description: 'Knight game with multiple knights', img: 'multiknight', gameType: GameTypeEnum.MULTI_KNIGHT },
 ];
